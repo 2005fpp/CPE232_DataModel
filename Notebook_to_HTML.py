c68e880352bf3d8a5a19fa62ddce710d3bbc25fa
Feen0305 <pheen.phoorin@gmail.com>
2025-03-06 11:13:29 +0700

    HW4 converter

--- Notebook_to_HTML.py
+++ Notebook_to_HTML.py
@@ -1,8 +1,8 @@
 from nbconvert import HTMLExporter
 import nbformat
 
-NOTEBOOK_PATH = 'WEEK4/HW3_Data_Preparation.ipynb'
-HTML_PATH = 'WEEK4/report/HW3_Data_Preparation.html'
+NOTEBOOK_PATH = 'WEEK7/HW_4_Data_Visualization_EDA.ipynb'
+HTML_PATH = 'WEEK7/reports/HW_4_Data_Visualization_EDA.html'
 
 # Load your notebook file with UTF-8 encoding
 with open(NOTEBOOK_PATH, encoding="utf-8") as f:
